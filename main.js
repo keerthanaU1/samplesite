297d6600cd23cea78dc17285532c313413f2a326
keerthanaU1 <123923510+keerthanaU1@users.noreply.github.com>
2023-07-02 13:33:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+//let openMenu=()=>{
+   // let sideBar=document.getElementById("sideBar");
+   // console.log("hello")
+   // let sideMenu=document.createElement("div");
+   // let sidemenuHeading=document.createElement("h1");
+    //sidemenuHeading.textContent="hello";
+    //sideMenu.appendChild(sidemenuHeading);
+    //sideBar.appendChild(sideMenu);
+//}
+function openNav() {
+    document.getElementById("mySidenav").style.width = "550px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
